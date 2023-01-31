4169d278e6cc0b2098c5d71f6a1e3f07e654098c
Ben Myers <ben.myers@catapultsystems.com>
2023-01-31 14:48:18 -0800

    buildout alerts api & state, switch to use dialog

--- src/webparts/clientInfoWebpart/components/ManageAlerts/ManageAlerts.tsx
+++ src/webparts/clientInfoWebpart/components/ManageAlerts/ManageAlerts.tsx
@@ -1,18 +1,40 @@
 import * as React from "react";
 import { useState, useEffect } from "react";
-import { Modal, IDragOptions } from "office-ui-fabric-react/lib/Modal";
-import { DefaultButton } from "office-ui-fabric-react/lib/Button";
+import {
+  Dialog,
+  DialogType,
+  DialogFooter,
+} from "office-ui-fabric-react/lib/Dialog";
+import { DefaultButton, PrimaryButton } from "office-ui-fabric-react/lib/Button";
+import { DetailsList, DetailsListLayoutMode, Selection, IColumn } from 'office-ui-fabric-react/lib/DetailsList'
 import { GlobalValues } from "../../Dataprovider/GlobalValue";
 import { sp } from "@pnp/sp";
 
+export interface ISubWeb {
+  Title: string;
+  Id: string;
+  ServerRelativeUrl: string;
+}
+
+export interface IUserInfo {
+  Id: number;
+  UserPrincipalName: string;
+}
+
 // parent container Manage Alerts component
 const ManageAlerts = ({ spContext, isAlertModalOpen, onAlertModalHide }) => {
-  const [subWebInfo, setSubWebInfo] = useState<any[]>([]);
+  const [subWebInfo, setSubWebInfo] = useState<ISubWeb[]>([]);
+  const [currentAlertsInfo, setCurrentAlertsInfo] = useState<object[]>([]);
+  const [currentUserId, setCurrentUserId] = useState<IUserInfo>();
 
   const hostUrl = window.location.host;
-  console.log("hosturl: ", hostUrl);
+  const alertsArrayInfo: object[] = [];
 
+  // console.log("hosturl: ", hostUrl);
+
+  // useEffect to get Subwebs
   useEffect(() => {
+    console.log("In getSubwebs useEffect");
     // get sub-portal information
     async function getSubwebs() {
       const subWebs = await sp.web
@@ -23,73 +45,77 @@ const ManageAlerts = ({ spContext, isAlertModalOpen, onAlertModalHide }) => {
       setSubWebInfo(subWebs);
     }
 
+    async function getCurrentUserId() {
+      const userId = await sp.web.currentUser();
+      setCurrentUserId(userId);
+    }
+
+    getCurrentUserId();
     getSubwebs();
   }, []);
 
   // will run only if subWebInfo is changed
   useEffect(() => {
-    if (subWebInfo.length > 0) {
+    if (subWebInfo.length > 0 && currentUserId.Id) {
+      console.log("In Alerts useEffect");
       // get current alerts set for user
       // TODO: grab ServerRelativeUrl from getSubwebs(), build below fetch with hostUrl var and ServerRelativeUrl to check if current user has an alert set on sub-portal (additional work to be done to check which list in sub-portal)
-      fetch(
-        `https://${hostUrl}${subWebInfo[0].ServerRelativeUrl}/_api/web/alerts`,
-        {
-          headers: {
-            Accept: "application/json;odata=verbose",
-          },
-        }
-      )
-        .then((data) => {
-          return data.json();
-        })
-        .then((alert) => {
-          console.log(alert);
-        });
+      subWebInfo.forEach((item) => {
+        fetch(
+          `https://${hostUrl}${item.ServerRelativeUrl}/_api/web/alerts?$filter=UserId eq ${currentUserId.Id}`,
+          {
+            headers: {
+              Accept: "application/json;odata=verbose",
+            },
+          }
+        )
+          .then((data) => {
+            return data.json();
+          })
+          .then((alert) => {
+            // console.log(alert);
+            if (alert.d.results.length > 0) {
+              alertsArrayInfo.push(alert);
+            }
+          });
+      });
+      setCurrentAlertsInfo(alertsArrayInfo);
     }
-  }, [subWebInfo]);
+  }, [subWebInfo, currentUserId]);
 
   // using to test state updates
   useEffect(() => {
     console.log(subWebInfo);
-  }, [subWebInfo]);
+    console.log(currentAlertsInfo);
+    console.log("currentUserId: ", currentUserId);
+  }, [subWebInfo, currentAlertsInfo, currentUserId]);
 
   return (
     <div>
-      <Modal
+      {/* <Modal
         isOpen={isAlertModalOpen}
         onDismiss={() => onAlertModalHide(true)}
         isBlocking={false}
         // containerClassName={styles.container}
         // dragOptions={this.state.isDraggable ? this._dragOptions : undefined}
+      > */}
+      <Dialog
+        hidden={!isAlertModalOpen}
+        onDismiss={() => onAlertModalHide(true)}
+        dialogContentProps={{
+          type: DialogType.normal,
+          title: "Alerts Management"
+        }}
+        modalProps={{
+          isBlocking: false,
+          styles: { main: { maxWidth: 700 } },
+        }}
       >
-        <DefaultButton onClick={() => onAlertModalHide(true)} text="Close" />
-        <div>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
-            lorem nulla, malesuada ut sagittis sit amet, vulputate in leo.
-            Maecenas vulputate congue sapien eu tincidunt. Etiam eu sem turpis.
-            Fusce tempor sagittis nunc, ut interdum ipsum vestibulum non. Proin
-            dolor elit, aliquam eget tincidunt non, vestibulum ut turpis. In hac
-            habitasse platea dictumst. In a odio eget enim porttitor maximus.
-            Aliquam nulla nibh, ullamcorper aliquam placerat eu, viverra et dui.
-            Phasellus ex lectus, maximus in mollis ac, luctus vel eros. Vivamus
-            ultrices, turpis sed malesuada gravida, eros ipsum venenatis elit,
-            et volutpat eros dui et ante. Quisque ultricies mi nec leo ultricies
-            mollis. Vivamus egestas volutpat lacinia. Quisque pharetra eleifend
-            efficitur.
-          </p>
-          <p>
-            Nam id mi justo. Nam vehicula vulputate augue, ac pretium enim
-            rutrum ultricies. Sed aliquet accumsan varius. Quisque ac auctor
-            ligula. Fusce fringilla, odio et dignissim iaculis, est lacus
-            ultrices risus, vitae condimentum enim urna eu nunc. In risus est,
-            mattis non suscipit at, mattis ut ante. Maecenas consectetur urna
-            vel erat maximus, non molestie massa consequat. Duis a feugiat nibh.
-            Sed a hendrerit diam, a mattis est. In augue dolor, faucibus vel
-            metus at, convallis rhoncus dui.
-          </p>
-        </div>
-      </Modal>
+        <DialogFooter>
+          <PrimaryButton onClick={() => onAlertModalHide(true)} text="Save" />
+          <DefaultButton onClick={() => onAlertModalHide(true)} text="Cancel" />
+        </DialogFooter>
+      </Dialog>
     </div>
   );
 };
