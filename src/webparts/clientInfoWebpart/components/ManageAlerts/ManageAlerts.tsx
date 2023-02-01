170233da6397ca48f70cd403f294a57de92794f5
Ben Myers <ben.myers@catapultsystems.com>
2023-02-01 15:57:06 -0800

    wire up detailsList & dropdown comps

--- src/webparts/clientInfoWebpart/components/ManageAlerts/ManageAlerts.tsx
+++ src/webparts/clientInfoWebpart/components/ManageAlerts/ManageAlerts.tsx
@@ -5,20 +5,53 @@ import {
   DialogType,
   DialogFooter,
 } from "office-ui-fabric-react/lib/Dialog";
-import { DefaultButton, PrimaryButton } from "office-ui-fabric-react/lib/Button";
-import { DetailsList, DetailsListLayoutMode, Selection, IColumn } from 'office-ui-fabric-react/lib/DetailsList'
+import {
+  DefaultButton,
+  PrimaryButton,
+} from "office-ui-fabric-react/lib/Button";
+import {
+  DetailsList,
+  DetailsListLayoutMode,
+  Selection,
+  IColumn,
+} from "office-ui-fabric-react/lib/DetailsList";
+import { MarqueeSelection } from "office-ui-fabric-react/lib/MarqueeSelection";
+import {
+  Dropdown,
+  DropdownMenuItemType,
+  IDropdownOption,
+} from "office-ui-fabric-react/lib/Dropdown";
 import { GlobalValues } from "../../Dataprovider/GlobalValue";
+import { mergeStyles } from "office-ui-fabric-react/lib/Styling";
 import { sp } from "@pnp/sp";
 
+const detailsListContainerStyles = mergeStyles({
+  height: 700,
+  overflowY: "scroll",
+});
+
+const alertSettingsContainerStyles = mergeStyles({
+  display: 'flex',
+  justifyContent: 'space-around',
+  padding: '15px 0 15px 0'
+});
+
+// for subwebs call
 export interface ISubWeb {
   Title: string;
   Id: string;
   ServerRelativeUrl: string;
 }
 
+// for currentUser info
 export interface IUserInfo {
   Id: number;
   UserPrincipalName: string;
+}
+
+export interface IDetailsListBasicExampleState {
+  items: ISubWeb[];
+  selectionDetails: {};
 }
 
 // parent container Manage Alerts component
@@ -26,11 +59,48 @@ const ManageAlerts = ({ spContext, isAlertModalOpen, onAlertModalHide }) => {
   const [subWebInfo, setSubWebInfo] = useState<ISubWeb[]>([]);
   const [currentAlertsInfo, setCurrentAlertsInfo] = useState<object[]>([]);
   const [currentUserId, setCurrentUserId] = useState<IUserInfo>();
+  const [items, setItems] = useState<ISubWeb[]>([]);
+  const [selectionDetails, setSelectionDetails] = useState<any>();
+  const [alertTypeItem, setAlertTypeItem] = useState<IDropdownOption>();
+  const [alertFrequencyItem, setAlertFrequencyItem] = useState<IDropdownOption>();
 
-  const hostUrl = window.location.host;
+  const hostUrl: string = window.location.host;
   const alertsArrayInfo: object[] = [];
 
-  // console.log("hosturl: ", hostUrl);
+  // TODO: Assess and complete the implementation of DetailsList
+  let selection: Selection;
+  let allItems = [];
+  let columns: IColumn[];
+
+  // column settings for data being displayed in DetailsList
+  columns = [
+    {
+      key: "column1",
+      name: "Sub-Portal Name",
+      fieldName: "Title",
+      minWidth: 100,
+      maxWidth: 200,
+      isResizable: true,
+    },
+    {
+      key: "column2",
+      name: "Id",
+      fieldName: "Id",
+      minWidth: 100,
+      maxWidth: 200,
+      isResizable: true,
+    },
+    {
+      key: "column2",
+      name: "Relative Path",
+      fieldName: "ServerRelativeUrl",
+      minWidth: 100,
+      maxWidth: 210,
+      isResizable: true,
+    },
+  ];
+
+  // TODO: Create function to use to set a selection
 
   // useEffect to get Subwebs
   useEffect(() => {
@@ -43,6 +113,7 @@ const ManageAlerts = ({ spContext, isAlertModalOpen, onAlertModalHide }) => {
         .orderBy("Created", false)();
       // console.table(subWebs);
       setSubWebInfo(subWebs);
+      setItems(subWebs);
     }
 
     async function getCurrentUserId() {
@@ -90,27 +161,96 @@ const ManageAlerts = ({ spContext, isAlertModalOpen, onAlertModalHide }) => {
     console.log("currentUserId: ", currentUserId);
   }, [subWebInfo, currentAlertsInfo, currentUserId]);
 
+  useEffect(() => {
+    console.log('Alert Type Item: ', alertTypeItem);
+    console.log('Alert Frequency Item: ', alertFrequencyItem);
+  }, [alertTypeItem, alertFrequencyItem]);
+
+  // TODO: Need to get selection details and formulate to write to list
+  const getSelectionDetails = () => {
+    const selectionCount = selection.getSelection();
+
+    console.log(selectionCount);
+  };
+
+  const onAlertTypeChange = (event: React.FormEvent<HTMLDivElement>, item: IDropdownOption): void => {
+    // console.log(`Selection change: ${item.text} ${item.selected ? 'selected' : 'unselected'}`);
+    // console.log('Alert Type Item: ', item);
+    setAlertTypeItem(item);
+  };
+
+  const onAlertFrequencyChange = (event: React.FormEvent<HTMLDivElement>, item: IDropdownOption): void => {
+    // console.log(`Selection change: ${item.text} ${item.selected ? 'selected' : 'unselected'}`);
+    // console.log('Alert Type Item: ', item);
+    setAlertFrequencyItem(item);
+  };
+
+  selection = new Selection({
+    onSelectionChanged: () => setSelectionDetails(getSelectionDetails()),
+  });
+
   return (
     <div>
-      {/* <Modal
-        isOpen={isAlertModalOpen}
-        onDismiss={() => onAlertModalHide(true)}
-        isBlocking={false}
-        // containerClassName={styles.container}
-        // dragOptions={this.state.isDraggable ? this._dragOptions : undefined}
-      > */}
       <Dialog
         hidden={!isAlertModalOpen}
         onDismiss={() => onAlertModalHide(true)}
+        minWidth={960}
         dialogContentProps={{
           type: DialogType.normal,
-          title: "Alerts Management"
+          title: "Alerts Management",
+          showCloseButton: true,
         }}
         modalProps={{
-          isBlocking: false,
-          styles: { main: { maxWidth: 700 } },
+          isBlocking: true,
+          // styles: { main: { maxHeight: 700, overflowY: 'scroll' } },
         }}
+        // styles={{ root: { maxHeight: 700 } }}
       >
+        <div className={detailsListContainerStyles}>
+          <MarqueeSelection selection={selection}>
+            <DetailsList
+              items={items}
+              columns={columns}
+              setKey="set"
+              // styles={{ root: { height: "500px" } }}
+              layoutMode={DetailsListLayoutMode.justified}
+              constrainMode={1}
+              selection={selection}
+              selectionPreservedOnEmptyClick={true}
+              ariaLabelForSelectionColumn="Toggle selection"
+              ariaLabelForSelectAllCheckbox="Toggle selection for all items"
+              checkButtonAriaLabel="Row checkbox"
+              // onItemInvoked={onItemInvoked}
+            />
+          </MarqueeSelection>
+        </div>
+        <div className={alertSettingsContainerStyles}>
+          <Dropdown
+            label="Alert Type"
+            selectedKey={alertTypeItem ? alertTypeItem.key : undefined}
+            onChange={onAlertTypeChange}
+            placeholder="Select an option"
+            options={[
+              { key: "allChanges", text: "All Changes" },
+              { key: "newItemsAdded", text: "New items are added" },
+              { key: "existingItemsModified", text: "Existing items are modified" },
+              { key: "itemsDeleted", text: "Items are deleted" }
+            ]}
+            styles={{ dropdown: { width: 300 } }}
+          />
+          <Dropdown
+            label="Alert Frequency"
+            selectedKey={alertFrequencyItem ? alertFrequencyItem.key : undefined}
+            onChange={onAlertFrequencyChange}
+            placeholder="Select an option"
+            options={[
+              { key: "immediately", text: "Send notification immediately" },
+              { key: "dailySummary", text: "Send a daily summary" },
+              { key: "weeklySummary", text: "Send a weekly summary" }
+            ]}
+            styles={{ dropdown: { width: 300 } }}
+          />
+        </div>
         <DialogFooter>
           <PrimaryButton onClick={() => onAlertModalHide(true)} text="Save" />
           <DefaultButton onClick={() => onAlertModalHide(true)} text="Cancel" />
