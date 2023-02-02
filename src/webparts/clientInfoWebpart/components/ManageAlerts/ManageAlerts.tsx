68b08dd02943cfe213b26829e06e4f62620acb49
Ben Myers <ben.myers@catapultsystems.com>
2023-02-02 13:28:44 -0800

    build loop and check in alert func to set items

--- src/webparts/clientInfoWebpart/components/ManageAlerts/ManageAlerts.tsx
+++ src/webparts/clientInfoWebpart/components/ManageAlerts/ManageAlerts.tsx
@@ -14,6 +14,7 @@ import {
   DetailsListLayoutMode,
   Selection,
   IColumn,
+  CheckboxVisibility
 } from "office-ui-fabric-react/lib/DetailsList";
 import { MarqueeSelection } from "office-ui-fabric-react/lib/MarqueeSelection";
 import {
@@ -31,13 +32,14 @@ const detailsListContainerStyles = mergeStyles({
 });
 
 const alertSettingsContainerStyles = mergeStyles({
-  display: 'flex',
-  justifyContent: 'space-around',
-  padding: '15px 0 15px 0'
+  display: "flex",
+  justifyContent: "space-around",
+  padding: "15px 0 15px 0",
 });
 
 // for subwebs call
 export interface ISubWeb {
+  key: string;
   Title: string;
   Id: string;
   ServerRelativeUrl: string;
@@ -62,10 +64,12 @@ const ManageAlerts = ({ spContext, isAlertModalOpen, onAlertModalHide }) => {
   const [items, setItems] = useState<ISubWeb[]>([]);
   const [selectionDetails, setSelectionDetails] = useState<any>();
   const [alertTypeItem, setAlertTypeItem] = useState<IDropdownOption>();
-  const [alertFrequencyItem, setAlertFrequencyItem] = useState<IDropdownOption>();
+  const [alertFrequencyItem, setAlertFrequencyItem] =
+    useState<IDropdownOption>();
 
   const hostUrl: string = window.location.host;
   const alertsArrayInfo: object[] = [];
+  const subWebsWithKey: object[] = [];
 
   // TODO: Assess and complete the implementation of DetailsList
   let selection: Selection;
@@ -91,7 +95,7 @@ const ManageAlerts = ({ spContext, isAlertModalOpen, onAlertModalHide }) => {
       isResizable: true,
     },
     {
-      key: "column2",
+      key: "column3",
       name: "Relative Path",
       fieldName: "ServerRelativeUrl",
       minWidth: 100,
@@ -101,6 +105,13 @@ const ManageAlerts = ({ spContext, isAlertModalOpen, onAlertModalHide }) => {
   ];
 
   // TODO: Create function to use to set a selection
+  // const setSelectionAlertItems = (alertItems: object[]) => {
+  //   // const subPortalType: string = '';
+
+  //   // console.log('alert items: ', alertItems);
+  //   console.log('subweb info items: ', subWebInfo);
+
+  // };
 
   // useEffect to get Subwebs
   useEffect(() => {
@@ -112,8 +123,14 @@ const ManageAlerts = ({ spContext, isAlertModalOpen, onAlertModalHide }) => {
         .select("Title", "ServerRelativeUrl", "Id")
         .orderBy("Created", false)();
       // console.table(subWebs);
-      setSubWebInfo(subWebs);
-      setItems(subWebs);
+
+      subWebs.forEach((subWebItem) => {
+        subWebItem.key = subWebItem.Id;
+      });
+
+
+      // setSubWebInfo(subWebs);
+      // setItems(subWebs);
     }
 
     async function getCurrentUserId() {
@@ -127,6 +144,10 @@ const ManageAlerts = ({ spContext, isAlertModalOpen, onAlertModalHide }) => {
 
   // will run only if subWebInfo is changed
   useEffect(() => {
+    let subPortalTypeName: string = '';
+    let subPortalTypeFunc: string = '';
+    let subPortalType: string = '';
+
     if (subWebInfo.length > 0 && currentUserId.Id) {
       console.log("In Alerts useEffect");
       // get current alerts set for user
@@ -144,49 +165,76 @@ const ManageAlerts = ({ spContext, isAlertModalOpen, onAlertModalHide }) => {
             return data.json();
           })
           .then((alert) => {
-            // console.log(alert);
             if (alert.d.results.length > 0) {
+              // grab 3-letter acronym (name) and 2 letter function (func) and combine to form I.e. AUD-WF
+              subPortalTypeName = item.ServerRelativeUrl.split('/')[3].split('-')[0];
+              subPortalTypeFunc = item.ServerRelativeUrl.split('/')[3].split('-')[1];
+              subPortalType = subPortalTypeName + '-' + subPortalTypeFunc;
+
+              console.log('subportal type: ', subPortalType);
+
+              if ((subPortalType === 'AUD-WF' || subPortalType === 'TAX-WF') && alert.d.results.length === 3) {
+                console.log('in set items in alerts call');
+
+                  selection.setKeySelected(item.Id, true, false);
+
+              }
+
               alertsArrayInfo.push(alert);
             }
+          })
+          .catch((error) => {
+            console.log(error);
+            throw new Error("There has been an error fetching Alerts Data");
           });
       });
       setCurrentAlertsInfo(alertsArrayInfo);
+      // setSelectionAlertItems(alertsArrayInfo);
     }
   }, [subWebInfo, currentUserId]);
 
   // using to test state updates
-  useEffect(() => {
-    console.log(subWebInfo);
-    console.log(currentAlertsInfo);
-    console.log("currentUserId: ", currentUserId);
-  }, [subWebInfo, currentAlertsInfo, currentUserId]);
+  // useEffect(() => {
+  //   // console.log(subWebInfo);
+  //   // console.log(currentAlertsInfo);
+  //   // console.log("currentUserId: ", currentUserId);
+  // }, [subWebInfo, currentAlertsInfo, currentUserId]);
 
-  useEffect(() => {
-    console.log('Alert Type Item: ', alertTypeItem);
-    console.log('Alert Frequency Item: ', alertFrequencyItem);
-  }, [alertTypeItem, alertFrequencyItem]);
+  // useEffect(() => {
+  //   console.log("Alert Type Item: ", alertTypeItem);
+  //   console.log("Alert Frequency Item: ", alertFrequencyItem);
+  // }, [alertTypeItem, alertFrequencyItem]);
 
-  // TODO: Need to get selection details and formulate to write to list
-  const getSelectionDetails = () => {
-    const selectionCount = selection.getSelection();
-
-    console.log(selectionCount);
-  };
-
-  const onAlertTypeChange = (event: React.FormEvent<HTMLDivElement>, item: IDropdownOption): void => {
+  // onChange function fired when user changes selection on Alert Type dropdown
+  const onAlertTypeChange = (
+    event: React.FormEvent<HTMLDivElement>,
+    item: IDropdownOption
+  ): void => {
     // console.log(`Selection change: ${item.text} ${item.selected ? 'selected' : 'unselected'}`);
     // console.log('Alert Type Item: ', item);
     setAlertTypeItem(item);
   };
 
-  const onAlertFrequencyChange = (event: React.FormEvent<HTMLDivElement>, item: IDropdownOption): void => {
+  // onChange function fired when user changes selection on Alert Frequency
+  const onAlertFrequencyChange = (
+    event: React.FormEvent<HTMLDivElement>,
+    item: IDropdownOption
+  ): void => {
     // console.log(`Selection change: ${item.text} ${item.selected ? 'selected' : 'unselected'}`);
     // console.log('Alert Type Item: ', item);
     setAlertFrequencyItem(item);
   };
 
+  // TODO: Need to get selection details and formulate to write to list
+  const getSelectionDetails = () => {
+    const selectionItems = selection.getSelection();
+
+    console.log(selectionItems);
+  };
+
   selection = new Selection({
     onSelectionChanged: () => setSelectionDetails(getSelectionDetails()),
+    getKey: (item: any) => item.Id
   });
 
   return (
@@ -211,6 +259,7 @@ const ManageAlerts = ({ spContext, isAlertModalOpen, onAlertModalHide }) => {
             <DetailsList
               items={items}
               columns={columns}
+              checkboxVisibility={CheckboxVisibility.always}
               setKey="set"
               // styles={{ root: { height: "500px" } }}
               layoutMode={DetailsListLayoutMode.justified}
@@ -233,20 +282,25 @@ const ManageAlerts = ({ spContext, isAlertModalOpen, onAlertModalHide }) => {
             options={[
               { key: "allChanges", text: "All Changes" },
               { key: "newItemsAdded", text: "New items are added" },
-              { key: "existingItemsModified", text: "Existing items are modified" },
-              { key: "itemsDeleted", text: "Items are deleted" }
+              {
+                key: "existingItemsModified",
+                text: "Existing items are modified",
+              },
+              { key: "itemsDeleted", text: "Items are deleted" },
             ]}
             styles={{ dropdown: { width: 300 } }}
           />
           <Dropdown
             label="Alert Frequency"
-            selectedKey={alertFrequencyItem ? alertFrequencyItem.key : undefined}
+            selectedKey={
+              alertFrequencyItem ? alertFrequencyItem.key : undefined
+            }
             onChange={onAlertFrequencyChange}
             placeholder="Select an option"
             options={[
               { key: "immediately", text: "Send notification immediately" },
               { key: "dailySummary", text: "Send a daily summary" },
-              { key: "weeklySummary", text: "Send a weekly summary" }
+              { key: "weeklySummary", text: "Send a weekly summary" },
             ]}
             styles={{ dropdown: { width: 300 } }}
           />
