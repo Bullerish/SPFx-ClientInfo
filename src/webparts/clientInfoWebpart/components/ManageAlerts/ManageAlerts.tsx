ed3e5eafd88d94b2f979f6dbdb707244a79582fd
Ben Myers <ben.myers@catapultsystems.com>
2023-02-10 14:16:55 -0800

    update existingAlerts, change behavior alertselect

--- src/webparts/clientInfoWebpart/components/ManageAlerts/ManageAlerts.tsx
+++ src/webparts/clientInfoWebpart/components/ManageAlerts/ManageAlerts.tsx
@@ -23,7 +23,7 @@ import {
   DropdownMenuItemType,
   IDropdownOption,
 } from "office-ui-fabric-react/lib/Dropdown";
-import { TextField } from 'office-ui-fabric-react/lib/TextField';
+import { TextField } from "office-ui-fabric-react/lib/TextField";
 // import { GlobalValues } from "../../Dataprovider/GlobalValue";
 import { mergeStyles } from "office-ui-fabric-react/lib/Styling";
 import { sp } from "@pnp/sp";
@@ -51,8 +51,8 @@ const alertSettingsContainerStyles = mergeStyles({
 });
 
 const filterControlStyles = mergeStyles({
-  margin: '0 30px 20px 0',
-  maxWidth: '300px'
+  margin: "0 30px 20px 0",
+  maxWidth: "300px",
 });
 
 // for subwebs call
@@ -60,7 +60,11 @@ export interface ISubWeb {
   key: string;
   Title: string;
   Id: string;
-  ServerRelativeUrl: string;
+  ServerRelativeUrl: string,
+  subPortalType: string,
+  typeOfSubPortal: string,
+  matterNumber: string,
+  alertId?: string;
 }
 
 // for currentUser info
@@ -84,14 +88,20 @@ const ManageAlerts = ({
   const [currentAlertsInfo, setCurrentAlertsInfo] = useState<object[]>([]);
   const [currentUserId, setCurrentUserId] = useState<IUserInfo>();
   const [items, setItems] = useState<ISubWeb[]>([]);
-  const [itemsToBeAddedForAlerts, setItemsToBeAddedForAlerts] = useState<ISubWeb[]>([]);
+  const [itemsToBeAddedForAlerts, setItemsToBeAddedForAlerts] = useState<
+    ISubWeb[]
+  >([]);
   const [selectionDetails, setSelectionDetails] = useState<any>([]);
   const [alertSelectedSubPortals, setAlertSelectedSubPortals] = useState<
-    string[]
+    ISubWeb[]
   >([]);
-  const [alertTypeItem, setAlertTypeItem] = useState<IDropdownOption>({key: 'allChanges', text: 'All Changes'});
-  const [alertFrequencyItem, setAlertFrequencyItem] =
-    useState<IDropdownOption>({key: 'immediately', text: 'Send notification immediately'});
+  const [alertTypeItem, setAlertTypeItem] = useState<IDropdownOption>({
+    key: "allChanges",
+    text: "All Changes",
+  });
+  const [alertFrequencyItem, setAlertFrequencyItem] = useState<IDropdownOption>(
+    { key: "immediately", text: "Send notification immediately" }
+  );
 
   const hostUrl: string = window.location.host;
   const absoluteUrl: string = spContext.pageContext._web.absoluteUrl;
@@ -99,11 +109,11 @@ const ManageAlerts = ({
 
   const userAlertsList = "UserAlertsList";
   const alertsArrayInfo: object[] = [];
-  const existingAlerts: object[] = [];
+  const existingAlerts: ISubWeb[] = [];
   const subWebsWithKey: ISubWeb[] = [];
 
   let selection: Selection;
-  let selectionForAlertsToAdd: Selection;
+  let selectionForAlertsToAdd: Selection = new Selection();
   let itemDetailsToBeSaved = [];
   let columns: IColumn[] = [
     {
@@ -130,20 +140,22 @@ const ManageAlerts = ({
       maxWidth: 210,
       isResizable: true,
     },
-  ];;
+  ];
 
-  // * useEffect to get Subwebs
+  // useEffect to get Subwebs
+  //
+  //
   useEffect(() => {
     let subPortalTypeName: string = "";
     let subPortalTypeFunc: string = "";
     let subPortalType: string = "";
     let determinesPortalType: boolean;
-    let typeOfSubPortal: string = '';
+    let typeOfSubPortal: string = "";
 
-    let matterNumber: string = '';
-    let matterPieceOne: string = '';
-    let matterPieceTwo: string = '';
-    let matterPieceThree: string = '';
+    let matterNumber: string = "";
+    let matterPieceOne: string = "";
+    let matterPieceTwo: string = "";
+    let matterPieceThree: string = "";
 
     console.log("In getSubwebs useEffect");
     // get sub-portal information
@@ -162,12 +174,16 @@ const ManageAlerts = ({
           subWebItem.ServerRelativeUrl.split("/")[3].split("-")[1];
         subPortalType = subPortalTypeName + "-" + subPortalTypeFunc;
         // check if string contains WF, if true then Workflow, if false then File Exchange
-        determinesPortalType = subPortalType.indexOf('WF') !== -1;
+        determinesPortalType = subPortalType.indexOf("WF") !== -1;
 
-        matterPieceOne = subWebItem.ServerRelativeUrl.split('/')[3].split('-')[2];
-        matterPieceTwo = subWebItem.ServerRelativeUrl.split('/')[3].split('-')[3];
-        matterPieceThree = subWebItem.ServerRelativeUrl.split('/')[3].split('-')[4];
-        matterNumber = matterPieceOne + '-' + matterPieceTwo + '-' + matterPieceThree;
+        matterPieceOne =
+          subWebItem.ServerRelativeUrl.split("/")[3].split("-")[2];
+        matterPieceTwo =
+          subWebItem.ServerRelativeUrl.split("/")[3].split("-")[3];
+        matterPieceThree =
+          subWebItem.ServerRelativeUrl.split("/")[3].split("-")[4];
+        matterNumber =
+          matterPieceOne + "-" + matterPieceTwo + "-" + matterPieceThree;
 
         if (
           subPortalType === "AUD-WF" ||
@@ -175,9 +191,9 @@ const ManageAlerts = ({
           subPortalType === "AUD-FE"
         ) {
           if (determinesPortalType === true) {
-            typeOfSubPortal = 'Workflow';
+            typeOfSubPortal = "Workflow";
           } else {
-            typeOfSubPortal = 'File Exchange';
+            typeOfSubPortal = "File Exchange";
           }
 
           let subWebItemWithKey = {
@@ -185,7 +201,7 @@ const ManageAlerts = ({
             key: subWebItem.Id,
             subPortalType: subPortalType,
             typeOfSubPortal: typeOfSubPortal,
-            matterNumber: matterNumber
+            matterNumber: matterNumber,
           };
           subWebsWithKey.push(subWebItemWithKey);
         }
@@ -193,7 +209,7 @@ const ManageAlerts = ({
 
       // console.log(subWebsWithKey);
       setSubWebInfo(subWebsWithKey);
-      console.log('refetch of subwebs occured::::');
+      console.log("refetch of subwebs occured::::");
       setItems(subWebsWithKey);
     }
 
@@ -206,13 +222,12 @@ const ManageAlerts = ({
     getSubwebs();
   }, []);
 
-  // * will run only if subWebInfo is changed/Contains API call to Alerts endpoint (fetches existing alerts)
+  // will run only if subWebInfo is changed/Contains API call to Alerts endpoint (fetches existing alerts)
   useEffect(() => {
     let subPortalTypeName: string = "";
     let subPortalTypeFunc: string = "";
     let subPortalType: string = "";
     let alertsToSet: string[] = [];
-
 
     if (subWebInfo.length > 0 && currentUserId.Id) {
       console.log("In Alerts useEffect");
@@ -238,7 +253,7 @@ const ManageAlerts = ({
                 item.ServerRelativeUrl.split("/")[3].split("-")[1];
               subPortalType = subPortalTypeName + "-" + subPortalTypeFunc;
 
-              console.log("subportal type: ", subPortalType);
+              // console.log("subportal type: ", subPortalType);
 
               if (
                 (subPortalType === "AUD-WF" || subPortalType === "TAX-WF") &&
@@ -246,19 +261,39 @@ const ManageAlerts = ({
               ) {
                 alertsToSet.push(item.Id);
                 // testing creating existing alert array
-                existingAlerts.push({ subPortId: item.Id, subPortRelativeUrl: item.ServerRelativeUrl, alertId: alert.d.results[0].ID });
-
+                existingAlerts.push({
+                  key: item.Id,
+                  Id: item.Id,
+                  Title: item.Title,
+                  ServerRelativeUrl: item.ServerRelativeUrl,
+                  subPortalType: item.subPortalType,
+                  typeOfSubPortal: item.typeOfSubPortal,
+                  matterNumber: item.matterNumber,
+                  alertId: alert.d.results[0].ID,
+                });
               } else if (
-                (subPortalType === "AUD-FE"/* || subPortalType === "ADV-FE"*/) &&
+                subPortalType === "AUD-FE" /* || subPortalType === "ADV-FE"*/ &&
                 alert.d.results.length === 1
-                ) {
-                  alertsToSet.push(item.Id);
-                  existingAlerts.push({ subPortId: item.Id, subPortRelativeUrl: item.ServerRelativeUrl, alertId: alert.d.results[0].ID });
+              ) {
+                alertsToSet.push(item.Id);
+                existingAlerts.push({
+                  key: item.Id,
+                  Id: item.Id,
+                  Title: item.Title,
+                  ServerRelativeUrl: item.ServerRelativeUrl,
+                  subPortalType: item.subPortalType,
+                  typeOfSubPortal: item.typeOfSubPortal,
+                  matterNumber: item.matterNumber,
+                  alertId: alert.d.results[0].ID,
+                });
               }
 
               alertsArrayInfo.push(alert);
 
-              setAlertSelectedSubPortals(alertsToSet);
+              setAlertSelectedSubPortals(existingAlerts); //alertsToSet
+              setItemsToBeAddedForAlerts(existingAlerts); //alertsToSet
+
+
             }
           })
           .catch((error) => {
@@ -266,42 +301,59 @@ const ManageAlerts = ({
             throw new Error("There has been an error fetching Alerts Data");
           });
       });
-      console.log('logging existingAlerts arr: ', existingAlerts);
+      console.log("logging existingAlerts arr: ", existingAlerts);
       setCurrentAlertsInfo(alertsArrayInfo);
       // setSelectionAlertItems(alertsArrayInfo);
     }
   }, [subWebInfo, currentUserId]);
 
-  // * for setting the pre-existing alerts on the DetailsList UI for user
+  // for setting the pre-existing alerts on the DetailsList UI for user
+  //
+  //
   useEffect(() => {
     if (isAlertModalOpen) {
       console.log("in If condition for isAlertModalOpen");
+
+      selectionForAlertsToAdd.setItems(itemsToBeAddedForAlerts, false);
+      selectionForAlertsToAdd.setAllSelected(true);
 
       setTimeout(() => {
         alertSelectedSubPortals.forEach((alertItem) => {
           // console.log(alertItem);
           // selection.setKeySelected(alertItem, true, false);
-          selection.setKeySelected(alertItem, true, false);
+          // selection.setKeySelected(alertItem.key, true, false);
         });
       }, 500);
     }
   }, [isAlertModalOpen]);
 
+  // fires when itemsToBeAddedForAlerts is changed
+  //
+  //
+  // useEffect(() => {
+  //   console.log("firing itemsToBeAddedForAlerts useEffect");
+  //   selectionForAlertsToAdd.setItems(itemsToBeAddedForAlerts, false);
+  //   selectionForAlertsToAdd.setAllSelected(true);
+
+  // }, [itemsToBeAddedForAlerts]);
+
   // only used for tracking/logging state values
+  //
+  //
   useEffect(() => {
     // console.log("in selectionDetails useEffect");
     // console.log(selectionDetails);
-    console.log('currentAlertsInfo: ', currentAlertsInfo);
-    console.log('alert selected subportals: ', alertSelectedSubPortals);
+    // console.log('currentAlertsInfo: ', currentAlertsInfo);
+    // console.log("alert selected subportals: ", alertSelectedSubPortals);
     // console.log('logging items array: ', items);
   });
 
   // TODO: Need to add Alerts to delete in the payload to the user list
   const addUserAlertsListItem = async () => {
     let listItem: object = {};
-    console.log('in AddUserAlertslistItem Func');
+    console.log("in AddUserAlertslistItem Func");
 
-    selectionDetails.forEach(el => {
+    selectionDetails.forEach((el) => {
       // console.log(el);
       itemDetailsToBeSaved.push(el.ServerRelativeUrl);
     });
@@ -313,53 +365,35 @@ const ManageAlerts = ({
       AbsoluteUrl: absoluteUrl,
       AlertType: alertTypeItem.key,
       AlertFrequency: alertFrequencyItem.key,
-      AlertsToAdd: itemDetailsToBeSaved.toString().replace(/,/g, ';'),
+      AlertsToAdd: itemDetailsToBeSaved.toString().replace(/,/g, ";"),
       // TODO: factor logic for items to be deleted and input data here similar to AlertsToAdd
-      AlertsToDelete: ''
+      AlertsToDelete: "",
     };
 
-    console.log('item details to be saved: ', listItem);
+    console.log("item details to be saved: ", listItem);
 
-    const itemAddResult: IItemAddResult = await sp.web.lists.getByTitle(userAlertsList).items.add(listItem);
+    const itemAddResult: IItemAddResult = await sp.web.lists
+      .getByTitle(userAlertsList)
+      .items.add(listItem);
 
-    console.log('itemAddResult: ', itemAddResult);
-
+    console.log("itemAddResult: ", itemAddResult);
   };
 
   // checks for UserAlertsList, if it doesn't exist it gets created then columns will be added
   const ensureAlertsListExists = async () => {
-    console.log(selectionDetails);
-
-    const alertsListEnsureResult = await sp.web.lists.ensure(
-      userAlertsList
-    );
+    // console.log(selectionDetails);
+    const alertsListEnsureResult = await sp.web.lists.ensure(userAlertsList);
 
     if (alertsListEnsureResult.created) {
       console.log("list was created somewhere!!!!!");
 
       // since list was newly created, need to add all the relevant columns/fields
-      const alertsToAddField: IFieldAddResult =
-        await sp.web.lists
-          .getByTitle(userAlertsList)
-          .fields.addMultilineText(
-            "AlertsToAdd",
-            6,
-            true,
-            false,
-            false,
-            true
-          );
-          const alertsToDeleteField: IFieldAddResult =
-        await sp.web.lists
-          .getByTitle(userAlertsList)
-          .fields.addMultilineText(
-            "AlertsToDelete",
-            6,
-            true,
-            false,
-            false,
-            true
-          );
+      const alertsToAddField: IFieldAddResult = await sp.web.lists
+        .getByTitle(userAlertsList)
+        .fields.addMultilineText("AlertsToAdd", 6, true, false, false, true);
+      const alertsToDeleteField: IFieldAddResult = await sp.web.lists
+        .getByTitle(userAlertsList)
+        .fields.addMultilineText("AlertsToDelete", 6, true, false, false, true);
       const UserPrincipalNameField: IFieldAddResult = await sp.web.lists
         .getByTitle(userAlertsList)
         .fields.addText("UserPrincipalName", 255);
@@ -373,21 +407,49 @@ const ManageAlerts = ({
         .getByTitle(userAlertsList)
         .fields.addText("AlertFrequency", 255);
 
-        addUserAlertsListItem();
+      addUserAlertsListItem();
     } else {
       console.log("list already existed!!!");
       addUserAlertsListItem();
     }
   };
 
-  // function that runs when the user enters text into the Filter text box
-  const onChangeFilterText = (ev: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>, text: string): void => {
+  // TODO: logic to process for determining if existing alerts are to be deleted
+  const factorAlertsToDelete = () => {
+    const itemsToDelete = itemsToBeAddedForAlerts.filter((obj) => {
+      console.log(obj);
+      return alertSelectedSubPortals.indexOf(obj) !== -1;
+    });
 
-    const output: any[] = subWebInfo.filter(obj => {
+    console.log("logging alertSelectedSubPortals: ", alertSelectedSubPortals);
+
+    console.log("itemsToDelete: ", itemsToDelete);
+  };
+
+  // EVENT HANDLERS BELOW
+  // TODO: finish working with transfer state from staged alerts to be set
+  const onActiveItemChanged = (
+    item: ISubWeb[],
+    index: number,
+    ev: React.FocusEvent<HTMLElement>
+  ) => {
+    transferToMainDetailsList();
+  };
+
+  // function that runs when the user enters text into the Filter text box
+  const onChangeFilterText = (
+    ev: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>,
+    text: string
+  ): void => {
+    const output: any[] = subWebInfo.filter((obj) => {
       return itemsToBeAddedForAlerts.indexOf(obj) === -1;
     });
 
-    setItems(text ? output.filter(i => i.Title.toLowerCase().indexOf(text) > -1) : output);
+    setItems(
+      text
+        ? output.filter((i) => i.Title.toLowerCase().indexOf(text) > -1)
+        : output
+    );
   };
 
   // onChange function fired when user changes selection on Alert Type dropdown
@@ -410,16 +472,15 @@ const ManageAlerts = ({
 
   // TODO: function to capture items selected by user and adds them to top DetailsList Component and removes them from bottom DetailsList component
   const transferToMainDetailsList = () => {
-
     const selectionItems = selectionForAlertsToAdd.getSelection();
     const selectionGetItems = selectionForAlertsToAdd.getItems();
 
     // console.log('loggin selectionGetItems: ', selectionGetItems);
     // console.log('newArrayForAddAlerts', newArrayForAddAlerts);
 
-    setItems(items => [...items, ...selectionItems as any[]]);
+    setItems((items) => [...items, ...(selectionItems as any[])]);
 
-    const output: any[] = selectionGetItems.filter(obj => {
+    const output: any[] = selectionGetItems.filter((obj) => {
       return selectionItems.indexOf(obj) === -1;
     });
 
@@ -427,7 +488,6 @@ const ManageAlerts = ({
 
     setItemsToBeAddedForAlerts(output);
   };
-
 
   // TODO: function to capture items selected by user and adds them to top DetailsList Component and removes them from bottom DetailsList component
   const transferToAddAlertsDetailsList = () => {
@@ -439,9 +499,12 @@ const ManageAlerts = ({
     // console.log('loggin selectionGetItems: ', selectionGetItems);
     // console.log('newArrayForAddAlerts', newArrayForAddAlerts);
 
-    setItemsToBeAddedForAlerts(itemsToBeAddedForAlerts => [...itemsToBeAddedForAlerts, ...selectionItems as any[]]);
+    setItemsToBeAddedForAlerts((itemsToBeAddedForAlerts) => [
+      ...itemsToBeAddedForAlerts,
+      ...(selectionItems as any[]),
+    ]);
 
-    const output: any[] = selectionGetItems.filter(obj => {
+    const output: any[] = selectionGetItems.filter((obj) => {
       return selectionItems.indexOf(obj) === -1;
     });
 
@@ -449,12 +512,13 @@ const ManageAlerts = ({
 
     setItems(output);
   };
+  // END EVENT HANDLERS
 
-   // TODO: selection handler for when user selects an item in the top DetailsList comp
-   selectionForAlertsToAdd = new Selection({
-    onSelectionChanged: () => transferToMainDetailsList(),
-    getKey: (item: any) => item.key,
-  });
+  // TODO: selection handler for when user selects an item in the top DetailsList comp
+  // selectionForAlertsToAdd = new Selection({
+  //   onSelectionChanged: () => transferToMainDetailsList(),
+  //   getKey: (item: any) => item.key,
+  // });
 
   // this selection controls what sub-portal items get added to the "Alerts to be Added" DetailsList at top of page
   selection = new Selection({
@@ -486,8 +550,9 @@ const ManageAlerts = ({
               columns={columns}
               checkboxVisibility={CheckboxVisibility.always}
               setKey="set"
+              onActiveItemChanged={onActiveItemChanged}
               onShouldVirtualize={() => false}
-              selectionMode={SelectionMode.single}
+              selectionMode={SelectionMode.multiple}
               // styles={{ root: { height: "500px" } }}
               layoutMode={DetailsListLayoutMode.justified}
               constrainMode={1}
@@ -500,7 +565,11 @@ const ManageAlerts = ({
             />
           </MarqueeSelection>
         </div>
-        <TextField label="Filter by Sub-Portal Name:" onChange={onChangeFilterText} className={filterControlStyles} />
+        <TextField
+          label="Filter by Sub-Portal Name:"
+          onChange={onChangeFilterText}
+          className={filterControlStyles}
+        />
         <div className={detailsListContainerStyles}>
           <MarqueeSelection selection={selection}>
             <DetailsList
@@ -555,7 +624,7 @@ const ManageAlerts = ({
           />
         </div>
         <DialogFooter>
-          <PrimaryButton onClick={ensureAlertsListExists} text="Save Alerts" />
+          <PrimaryButton onClick={factorAlertsToDelete} text="Save Alerts" />
           <DefaultButton onClick={() => onAlertModalHide(true)} text="Cancel" />
         </DialogFooter>
       </Dialog>
