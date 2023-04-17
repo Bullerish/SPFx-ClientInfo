11b3488fabeb81829ba690d5051b01b611980c2d
Ben Myers <ben.myers@catapultsystems.com>
2023-04-17 12:29:49 -0700

    added sector and other interest checkbox sections

--- src/webparts/clientInfoWebpart/components/ClientProfileInfo/ClientProfileInfo.tsx
+++ src/webparts/clientInfoWebpart/components/ClientProfileInfo/ClientProfileInfo.tsx
@@ -9,15 +9,6 @@ import {
   DefaultButton,
   PrimaryButton,
 } from "office-ui-fabric-react/lib/Button";
-import {
-  DetailsList,
-  DetailsListLayoutMode,
-  Selection,
-  IColumn,
-  CheckboxVisibility,
-  SelectionMode,
-  IObjectWithKey,
-} from "office-ui-fabric-react/lib/DetailsList";
 import { MarqueeSelection } from "office-ui-fabric-react/lib/MarqueeSelection";
 import {
   Dropdown,
@@ -40,7 +31,8 @@ import { GlobalValues } from "../../Dataprovider/GlobalValue";
 import { setBaseUrl } from "office-ui-fabric-react";
 import { Pivot, PivotItem } from "office-ui-fabric-react/lib/Pivot";
 import { Stack, IStackProps } from "office-ui-fabric-react/lib/Stack";
-import { Checkbox } from 'office-ui-fabric-react/lib/Checkbox';
+import { Checkbox } from "office-ui-fabric-react/lib/Checkbox";
+
 
 interface IDropdownControlledState {
   jobFunctionItem?: { key: string | number | undefined };
@@ -53,77 +45,103 @@ interface IServices {
 }
 
 const jobFunctionOptions = [
-  { key: 'Accounting', text: 'Accounting' },
-  { key: 'Administration', text: 'Administration' },
-  { key: 'Analytics', text: 'Analytics' },
-  { key: 'Compliance', text: 'Compliance' },
-  { key: 'Executive', text: 'Executive' },
-  { key: 'Finance', text: 'Finance' },
-  { key: 'Human Resources', text: 'Human Resources' },
-  { key: 'IT', text: 'IT' },
-  { key: 'Legal', text: 'Legal' },
-  { key: 'Operations', text: 'Operations' },
-  { key: 'Risk', text: 'Risk' },
-  { key: 'Sales/Marketing', text: 'Sales/Marketing' },
-  { key: 'Tax', text: 'Tax' },
-  { key: 'Other', text: 'Other' },
+  { key: "Accounting", text: "Accounting" },
+  { key: "Administration", text: "Administration" },
+  { key: "Analytics", text: "Analytics" },
+  { key: "Compliance", text: "Compliance" },
+  { key: "Executive", text: "Executive" },
+  { key: "Finance", text: "Finance" },
+  { key: "Human Resources", text: "Human Resources" },
+  { key: "IT", text: "IT" },
+  { key: "Legal", text: "Legal" },
+  { key: "Operations", text: "Operations" },
+  { key: "Risk", text: "Risk" },
+  { key: "Sales/Marketing", text: "Sales/Marketing" },
+  { key: "Tax", text: "Tax" },
+  { key: "Other", text: "Other" },
 ];
 
 const jobRoleOptions = [
-  { key: 'Board Member', text: 'Board Member' },
-  { key: 'Csuite', text: 'Csuite' },
-  { key: 'VP / Director', text: 'VP / Director' },
-  { key: 'Manager', text: 'Manager' },
-  { key: 'Staff', text: 'Staff' },
-  { key: 'Student', text: 'Student' },
-  { key: 'Other', text: 'Other' },
+  { key: "Board Member", text: "Board Member" },
+  { key: "Csuite", text: "Csuite" },
+  { key: "VP / Director", text: "VP / Director" },
+  { key: "Manager", text: "Manager" },
+  { key: "Staff", text: "Staff" },
+  { key: "Student", text: "Student" },
+  { key: "Other", text: "Other" },
 ];
 
 const columnProps: Partial<IStackProps> = {
   tokens: { childrenGap: 15 },
-  styles: { root: { width: 300 } }
+  styles: { root: { width: 300 } },
 };
 
 const profileContainerProps: Partial<IStackProps> = {
-  styles: { root: { width: 700, marginTop: 20 } }
+  styles: { root: { width: 700, marginTop: 20 } },
 };
 
 const textAreasProps: Partial<IStackProps> = {
-  styles: { root: { width: 650 } }
+  styles: { root: { width: 650 } },
 };
-
-
 
 const ClientProfileInfo = ({
   spContext,
   isClientProfileInfoModalOpen,
   onClientProfileInfoModalHide,
 }): React.ReactElement => {
-  const [jobFunctionItem, setJobFunctionItem] = useState({ key: undefined, text: '' });
+  const [jobFunctionItem, setJobFunctionItem] = useState({
+    key: undefined,
+    text: "",
+  });
 
-  // for checkboxes
+  // for services checkboxes
   const [services, setServices] = useState<IServices[]>([
-    {text: 'Accounting/Assurance', checked: false},
-    {text: 'Advisory', checked: false},
-    {text: 'Private Client Services', checked: false},
-    {text: 'Solutions for C-Suite Executives', checked: false},
-    {text: 'Tax', checked: false},
+    { text: "Accounting/Assurance", checked: false },
+    { text: "Advisory", checked: false },
+    { text: "Private Client Services", checked: false },
+    { text: "Solutions for C-Suite Executives", checked: false },
+    { text: "Tax", checked: false },
+  ]);
+
+  // for sectors checkboxes
+  const [sectors, setSectors] = useState<IServices[]>([
+    { text: "Affordable Housing", checked: false },
+    { text: "Construction", checked: false },
+    { text: "Commercial Real Estate", checked: false },
+    { text: "Financial Services", checked: false },
+    { text: "Cannabis", checked: false },
+    { text: "Healthcare", checked: false },
+    { text: "Life Sciences", checked: false },
+    { text: "Manufacturing and Distribution", checked: false },
+    { text: "Not for Profit and Education", checked: false },
+    { text: "Private Equity/Other Financial Sponsors", checked: false },
+    { text: "Renewable Energy", checked: false },
+    { text: "Retail", checked: false },
+    { text: "Government Contracting", checked: false },
+    { text: "CanGovernment - Audit/Accountingnabis", checked: false },
+    { text: "Government - Compliance and Monitoring", checked: false },
+    { text: "Government - Emergency Management", checked: false },
+  ]);
+
+  // for other interests checkboxes
+  const [otherInterests, setOtherInterests] = useState<IServices[]>([
+    { text: "Alumni Events", checked: false },
+    { text: "CPE Offerings", checked: false },
+    { text: "Executive Women's Forum Events", checked: false },
   ]);
 
   useEffect(() => {
-    console.log('triggering useEffect services');
+    console.log("triggering useEffect services");
     console.log(services);
   }, [services]);
 
-
   // set services checkboxes and state
   const onServicesChange = (text: string, value) => {
-
     let servicesTempArr = services;
 
     servicesTempArr.forEach((e) => {
       if (text === e.text && value) {
-        console.log('inside foreach logging val:: ', e);
+        console.log("inside foreach logging val:: ", e);
         e.checked = true;
       }
 
@@ -135,6 +153,39 @@ const ClientProfileInfo = ({
     setServices([...servicesTempArr]);
   };
 
+   // set services checkboxes and state
+   const onSectorsChange = (text: string, value) => {
+    let sectorsTempArr = sectors;
+
+    sectorsTempArr.forEach((e) => {
+      if (text === e.text && value) {
+        e.checked = true;
+      }
+
+      if (text === e.text && !value) {
+        e.checked = false;
+      }
+    });
+
+    setSectors([...sectorsTempArr]);
+  };
+
+  // set other interests checkboxes and state
+  const onOtherInterestsChange = (text: string, value) => {
+    let otherInterestsTempArr = otherInterests;
+
+    otherInterestsTempArr.forEach((e) => {
+      if (text === e.text && value) {
+        e.checked = true;
+      }
+
+      if (text === e.text && !value) {
+        e.checked = false;
+      }
+    });
+
+    setOtherInterests([...otherInterestsTempArr]);
+  };
 
   return (
     <div>
@@ -150,11 +201,12 @@ const ClientProfileInfo = ({
         modalProps={{
           isBlocking: true,
           // styles: { main: { maxHeight: 700, overflowY: 'scroll' } },
-          className: styles.manageAlerts,
+          className: styles.clientProfileInfo,
         }}
         // styles={{ root: { maxHeight: 700 } }}
       >
         <Pivot>
+          {/* PROFILE TAB */}
           <PivotItem
             headerText="Profile"
             headerButtonProps={{
@@ -163,68 +215,133 @@ const ClientProfileInfo = ({
             }}
           >
 
-            {/* <div style={{ display: 'flex', justifyContent: 'center', width: '100%' }}> */}
             <Stack gap={20} horizontalAlign="center" {...profileContainerProps}>
-              <Stack horizontal horizontalAlign="center" tokens={{ childrenGap: 50 }} styles={{ root: { width: 650 } }}>
-
-
+              <Stack
+                horizontal
+                horizontalAlign="center"
+                tokens={{ childrenGap: 50 }}
+                styles={{ root: { width: 650 } }}
+              >
                 <Stack {...columnProps}>
-
                   <TextField label="Full Name" />
 
                   <Dropdown
                     label="Job Role"
-                    selectedKey={jobFunctionItem ? jobFunctionItem.key : undefined}
+                    selectedKey={
+                      jobFunctionItem ? jobFunctionItem.key : undefined
+                    }
                     // onChange={this._onChange}
                     placeholder="Select an option"
                     options={jobRoleOptions}
                     styles={{ dropdown: { width: 300 } }}
                   />
-
                 </Stack>
 
-                <Stack {...columnProps} >
-
+                <Stack {...columnProps}>
                   <TextField label="Title" />
 
                   <Dropdown
                     label="Job Function"
-                    selectedKey={jobFunctionItem ? jobFunctionItem.key : undefined}
+                    selectedKey={
+                      jobFunctionItem ? jobFunctionItem.key : undefined
+                    }
                     // onChange={this._onChange}
                     placeholder="Select an option"
                     options={jobFunctionOptions}
                     styles={{ dropdown: { width: 300 } }}
                   />
-
                 </Stack>
-
-
-
               </Stack>
 
               <Stack gap={20} {...textAreasProps}>
-
                 <TextField label="Mailing Address" multiline rows={4} />
-                <TextField label="What are your ESG Interests?" multiline rows={4} />
-                <TextField label="Do you hold any board positions, if so which boards?" multiline rows={4} />
-                <TextField label="What are you passionate about?" multiline rows={4} />
-
+                <TextField
+                  label="What are your ESG Interests?"
+                  multiline
+                  rows={4}
+                />
+                <TextField
+                  label="Do you hold any board positions, if so which boards?"
+                  multiline
+                  rows={4}
+                />
+                <TextField
+                  label="What are you passionate about?"
+                  multiline
+                  rows={4}
+                />
               </Stack>
             </Stack>
-            {/* </div> */}
 
           </PivotItem>
-          <PivotItem headerText="Subscriptions">
+          {/* SUBSCRIPTIONS TAB */}
+          <PivotItem headerText="Subscriptions" className={styles.marginTabsTop}>
 
-            <div>SERVICES</div>
+            <div>
 
-            {services.map(e => (
-              <Checkbox label={e.text} checked={e.checked} onChange={(ev, value) => onServicesChange(e.text, value)} />
-            ))}
+              <div className={styles.subscriptionGuidanceText}>
+                <Text variant="medium">Please choose the areas in which you would like to subscribe to learn more:</Text>
+              </div>
+
+              <Text variant="mediumPlus">SERVICES</Text>
+
+              <div className={styles.checkboxFlexStyles}>
+
+                {services.map((e) => (
+                  <Checkbox
+                  label={e.text}
+                  checked={e.checked}
+                  onChange={(ev, value) => onServicesChange(e.text, value)}
+                  />
+                ))}
+
+              </div>
+
+            </div>
+
+            <div>
+              <Text variant="mediumPlus">SECTORS</Text>
+
+              <div className={styles.checkboxFlexStyles}>
+
+                {sectors.map((e) => (
+                  <Checkbox
+                  label={e.text}
+                  checked={e.checked}
+                  onChange={(ev, value) => onSectorsChange(e.text, value)}
+                  />
+                ))}
+
+              </div>
+            </div>
+
+            <div>
+              <Text variant="mediumPlus">OTHER INTERESTS</Text>
+
+              <div className={styles.checkboxFlexStyles}>
+
+                {otherInterests.map((e) => (
+                  <Checkbox
+                  label={e.text}
+                  checked={e.checked}
+                  onChange={(ev, value) => onOtherInterestsChange(e.text, value)}
+                  />
+                ))}
+              </div>
+            </div>
 
           </PivotItem>
-          <PivotItem headerText="Contacts">
-            <div>Contacts</div>
+          {/* CONTACTS TAB */}
+          <PivotItem headerText="Contacts" className={styles.marginTabsTop}>
+
+            <div>
+              <TextField
+                label="Please share the names and emails of your team members who will be working with CohnReznick"
+                multiline
+                rows={6}
+              />
+            </div>
+
           </PivotItem>
         </Pivot>
         <DialogFooter>
