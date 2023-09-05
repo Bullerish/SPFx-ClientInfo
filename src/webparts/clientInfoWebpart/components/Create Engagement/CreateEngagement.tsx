0d0f56231df4a58a5e015e59f88237cdcefbcef3
rdicicco <49653021+rdicicco@users.noreply.github.com>
2023-09-05 16:40:41 -0600

    remove k1 file upload if user clicks back button

--- src/webparts/clientInfoWebpart/components/Create Engagement/CreateEngagement.tsx
+++ src/webparts/clientInfoWebpart/components/Create Engagement/CreateEngagement.tsx
@@ -2692,7 +2692,7 @@ class CreateEngagement extends React.Component<ICreateEngagement> {
     }
 
     private previousDialog(e) {
-        if (this.state.currentScreen == "screen2" || this.state.currentScreen == "screen5") {
+        if (this.state.currentScreen == "screen2") {
             this.setState({
                 validate: false,
                 IsDuplicate: false,
@@ -2720,6 +2720,19 @@ class CreateEngagement extends React.Component<ICreateEngagement> {
                 currentScreen: "screen3",
                 dialogbuttonname: "Next",
                 titleText: "- Template and Provisioning",
+            });
+        }
+        if (this.state.currentScreen == "screen5") {
+            // clear the file input onload
+            this.setState({
+                K1FileName: "",
+                K1Errors: [],
+                validate: false,
+                IsDuplicate: false,
+                currentScreen: "screen1",
+                dialogbuttonname: "Next",
+                titleText: "",
+
             });
         }
     }
@@ -2973,7 +2986,12 @@ class CreateEngagement extends React.Component<ICreateEngagement> {
                     }
                 }
             }
-            else if (this.state.PortalTypeSelected == 'K1') {
+            else if (this.state.PortalTypeSelected == 'K1') {                
+                if ((this.state.K1FileName == "") || (this.state.validate == true)) {
+                    this.setState({
+                        validate: true
+                    });
+                }
                 if ((this.state.K1FileName == "") || (this.state.validate == true)) {
                     this.setState({
                         validate: true
